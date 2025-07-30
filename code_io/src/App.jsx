585f2ab6d7e6ce08b28fa8b54d724f67432dd9a2
venkatalakshmi14 <saransivakumar104@gmail.com>
2025-07-30 21:59:23 +0530

    new react component is created

---
+++ code_io/src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar'
+import Course from './components/Course'
+import Count from './components/Count'
+  
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <Course/>
+      <Count/>
+    </>
+  )
+}
+
+export default App
---
+++ code_io/src/components/Course.jsx
@@ -0,0 +1,13 @@
+const course1="HTML"
+function Course(){
+    return(
+        <div class="card">
+            <img src="" alt="" />
+            <h3>{2+5}</h3>
+            <p>This is html course</p>
+        </div>
+
+    );
+}
+
+export default Course;
